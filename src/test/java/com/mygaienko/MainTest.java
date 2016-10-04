a2d1a68eff9f28117387011e9cf8dccaf9b60779
dmygaenko <dmygaenko@gmail.com>
2016-10-04 16:07:58 +0300

    to commit

---
+++ src/test/java/com/mygaienko/MainTest.java
@@ -0,0 +1,41 @@
+package com.mygaienko;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Created by dmygaenko on 04/10/2016.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MainTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        this.mockMvc.perform(get("/login")
+                .accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk());
+    }
+}
