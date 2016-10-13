19231264645bb561a887602324e32d38b67fcefc
dmygaien <dmygaenko@gmail.com>
2016-10-14 02:51:29 +0300

    export example

--- src/main/java/com/mygaienko/model/BEntity.java
+++ src/main/java/com/mygaienko/model/BEntity.java
@@ -1,23 +1,16 @@
 package com.mygaienko.model;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Created by enda1n on 13.10.2016.
  */
+@Data
+@RequiredArgsConstructor
 public class BEntity {
 
-    private String b1;
-    private String b2;
+    private final String b1;
+    private final String b2;
 
-    public BEntity(String a1, String a2) {
-        this.b1 = a1;
-        this.b2 = a2;
-    }
-
-    public String getB1() {
-        return b1;
-    }
-
-    public String getB2() {
-        return b2;
-    }
 }
